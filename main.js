a0d554a334da10967e7a50682673e8500b774a70
meeky92 <112586695+meeky92@users.noreply.github.com>
2022-11-05 15:05:17 -0500

    works

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-// API key: abe153b769bca759b27c76dd4e984723
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
+import { myAPIKey } from "./API_key.js";
 
 {
-    const key = "abe153b769bca759b27c76dd4e984723";
-
     let form = document.getElementById('search');
-    console.log(form);
+    // console.log(form);
 
     async function handleSubmit(e){
         e.preventDefault();
-        console.log(e);
+        // console.log(e);
         let city = e.target.city.value;
         console.log(city);
         let cityInfo = await getWeather(city);
-        console.log(cityInfo);
         buildWeather(cityInfo);
-        console.log(cityInfo);
-
+        // console.log(cityInfo);
         e.target.city.value = '';
     }
 
     async function getWeather(city){
-        let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`);
+        let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ myAPIKey }&units=imperial`);
         console.log(response);
         let data = await response.json();
         return data
@@ -43,7 +37,6 @@
         feelsLikeBody.innerHTML = `${cityObj.main.feels_like}\u00B0 F`;
 
     }
-
 
     form.addEventListener('submit', handleSubmit);
 }
